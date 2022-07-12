e4e86e72e15da3bed074fbf64c27a747c559f9dc
naomischlosser <naomi_schlosser@hotmail.com>
2022-07-12 18:05:22 +0100

    Solution to step 8 exercise 1

---
+++ directory_step8.rb
@@ -0,0 +1,35 @@
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+
+  students = []
+  name = gets.chomp
+
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+
+  students
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(names)
+  names.each_with_index do |name, i|
+    puts "#{i + 1}. #{name[:name]} (#{name[:cohort]} cohort)"
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
